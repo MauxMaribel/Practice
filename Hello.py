d3eae931c608fa2a31ee494ee29d5218f908b84a
MauxMaribel <mauxmaribel@gmail.com>
2016-10-13 18:01:33 -0400

    added hello

---
+++ Hello.py
@@ -0,0 +1,10 @@
+print "Hello Squee <3. Just Making a practice file to make sure git is working."
+print "If it helps I can list some of the git shell commands to help remind you"
+print "of how to commit, push, and pull our files here."
+
+
+print """\n\tYou can also write out all the writing in these \"\"\" marks.
+	\nIt's super convienent when you have a ton of things you want to say"""
+	
+print
+print
